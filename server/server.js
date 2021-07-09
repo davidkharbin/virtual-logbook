c57b78aabfc9b61ad957b77f3bfe4e2023ac671a
davidkharbin <david_k_harbin@hotmail.com>
2021-07-08 20:27:14 -0600

    build: removed bodyParser (no longer required)

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+// lets me have environment variables in the .env file
 require('dotenv').config();
 
+// creates the express server & port
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware to parse json
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
+//
+app.use(express.static(path.join(__dirname, '../public/')));
 
-
-
-const PORT = process.env.PORT || 3000;
-
+// starts the server (listening)
 app.listen(PORT, () => {
   console.log(`Web server running on: http://localhost:${PORT}`);
 });
