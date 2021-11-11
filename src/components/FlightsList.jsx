3ba9410f479c6777773e1c3416230d8a6cdd870f
davidkharbin <david_k_harbin@hotmail.com>
2021-11-10 22:52:50 -0700

    refactor: functional component

--- src/components/FlightsList.jsx
+++ src/components/FlightsList.jsx
@@ -1,74 +1,47 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Flight from './Flight.jsx';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Flight from './Flight.jsx'
 
-export default class FlightsList extends Component {
-  constructor(props) {
-    super(props);
+const FlightsList = () => {
+  const [flights, setFlights] = useState([])
 
-    this.deleteFlight = this.deleteFlight.bind(this)
-
-    this.state = {flights: []};
-  }
-
-  componentDidMount() {
+  const getFlights = () => {
     axios.get('http://localhost:4000/flights/')
       .then(response => {
-        this.setState({ flights: response.data })
+        setFlights(response.data)
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error)
       })
   }
 
-  deleteFlight(id) {
-    axios.delete('http://localhost:4000/flights/'+id)
-      .then(response => { console.log(response.data)});
+  useEffect(() => {
+    console.log('ran')
+    getFlights()
+  }, [])
 
-    this.setState({
-      flights: this.state.flights.filter(el => el._id !== id)
-    })
-  }
-
-  flightList() {
-    return this.state.flights.map(currentflight => {
-      return <Flight flight={currentflight} deleteFlight={this.deleteFlight} key={currentflight._id}/>;
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Logged Flights</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Pilot name</th>
-              <th>Date</th>
-              <th>Duration</th>
-              <th>Landings</th>
-              <th>Departure Airport</th>
-              <th>Remarks</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.flightList() }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3>Logged Flights</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Pilot name</th>
+            <th>Date</th>
+            <th>Duration</th>
+            <th>Landings</th>
+            <th>Departure Airport</th>
+            <th>Remarks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {flights.map(currentflight => {
+            return <Flight flight={currentflight} key={currentflight._id} />
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
-
-
-
-
-// {
-//     "pilotname": "test",
-//     "date" : "2021-07-11T17:21:25.011+00:00",
-//     "duration": 120,
-//     "landings": 10,
-//     "departure_airport": "KARR",
-//     "remarks": "Touch and go's"
-// }
+export default FlightsList
